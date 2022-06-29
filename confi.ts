ca762f6734da4c413009eac14b8a5c41e605a3b9
sanjithsanjeev <sanjithsanjeevk@gmail.com>
2022-06-29 12:26:49 +0530

    screeshot and html report

--- confi.ts
+++ confi.ts
@@ -1,22 +1,74 @@
 
 
 exports.config = {
-    directConnect: true,
-  
-    // Capabilities to be passed to the webdriver instance.
-    capabilities: {
-      browserName: 'firefox'
-    },
+  directConnect: true,
 
-    onPrepare: async () => {
-      await browser.waitForAngularEnabled(false);
-      
-     },
-  
-    // Framework to use. Jasmine 
-    framework: 'jasmine',
-    specs:['./spec.ts'],
-  
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    browserName: 'firefox'
+  },
+
+  onPrepare: async () => {
+    await browser.waitForAngularEnabled(false);
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: './',
+      filePrefix: 'xmlresults'
+    }));
+
+    var fs = require('fs-extra');
+ 
+fs.emptyDir('screenshots/', function (err) {
+        console.log(err);
+    });
+ 
+    jasmine.getEnv().addReporter({
+        specDone: function(result) {
+            if (result.status == 'failed') {
+                browser.getCapabilities().then(function (caps) {
+                    var browserName = caps.get('browserName');
+ 
+                    browser.takeScreenshot().then(function (png) {
+                        var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName+ '.png');
+                        stream.write(new Buffer(png, 'base64'));
+                        stream.end();
+                    });
+                });
+            }
+        }
+    });
+  },
+
+  // Framework to use. Jasmine 
+  framework: 'jasmine',
+  specs: ['./spec.ts'],
+
+  onComplete: function () {
+    var browserName, browserVersion, platform, testConfig;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('protractor-html-reporter-2');
+
+      testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: './',
+        outputFilename: 'ProtractorTestReport',
+        screenshotPath: './screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+        testPlatform: platform
+      };
+      new HTMLReport().from('xmlresults.xml', testConfig);
+    });
+  }
 
 
 }
